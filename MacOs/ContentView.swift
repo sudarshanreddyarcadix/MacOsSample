9e69c5c0a57c243b001bfa927952f47ccb5fc328
sudarshanreddyarcadix <sudarshan.reddy@arcadix.com>
2022-04-06 18:16:29 +0530

    First Commit

---
+++ MacOs/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  MacOs
+//
+//  Created by admin on 06/04/22.
+//
+
+import SwiftUI
+
+struct Option : Hashable{
+    let title : String
+    let imageName : String
+}
+
+struct ContentView: View {
+    let options: [Option] = [
+        .init(title: "Home", imageName: "house"),
+        .init(title: "About", imageName: "info.circle"),
+        .init(title: "Settings", imageName: "gear"),
+        .init(title: "Social", imageName: "message")
+    ]
+    var body: some View {
+        NavigationView{
+            ListView(options: options)
+            
+            MainView()
+        }
+        .frame(minWidth:600, minHeight: 400)
+    }
+}
+
+struct ListView : View {
+    let options: [Option]
+    var body: some View{
+        VStack{
+            ForEach(options, id: \.self){ option in
+                HStack{
+                    Image(systemName: option.imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:20)
+                    
+                    Text(option.title)
+                    
+                    Spacer()
+                }
+                .padding(8)
+            }
+        }
+        
+        Spacer()
+    }
+}
+
+struct MainView : View {
+    var body: some View{
+        Text("List")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacOs/MacOsApp.swift
@@ -0,0 +1,22 @@
+//
+//  MacOsApp.swift
+//  MacOs
+//
+//  Created by admin on 06/04/22.
+//
+
+import Cocoa
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+
+import SwiftUI
+
+@main
+struct MacOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
